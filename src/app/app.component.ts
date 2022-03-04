9de30179f420fc0b008f70c8af90bfa79dcbe822
321-mark-123 <mark.first.name@gmail.com>
2022-03-04 21:18:17 +0100

    start slide show add img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mabedev';
+export class AppComponent implements OnInit { //implements OnInit == Onload
+  imgs = ['binaercode.jpg', 'brain.jpg', 'codescreen.jpg'];
+  currentImg = 0;
+  showImg = true
+
+  ngOnInit() {
+    this.updateImg();
+
+}
+
+  updateImg() {
+
+    setInterval(() => {
+      this.currentImg++; // addiere 1
+      this.currentImg = this.currentImg % this.imgs.length; // modulo wemm lenght = 3 rest dann null0 current img = 0
+      this.showImg = false
+
+      setTimeout(() => {
+      this.showImg = true
+      }, 100);
+
+}, 8000);
+   }
+
 }
